0f7ad0ce4c081bc6968dd9ac29a7136caa1cd88f
NathanGlarenth <ganzkevin65@gmail.com>
2024-11-10 20:34:28 +0700

    Create setadmin.lua
    setadmin.lua

---
+++ setadmin.lua
@@ -0,0 +1,47 @@
+-Testing File decompiled by pablo
+
+decompaile for terminator******
+
+decompiler tt: https://www.tiktok.com/jereell
+
+Local var 0_0 = require("lib.samp.events"]
+
+local var 01 false local var 0_2=100
+
+function var 0.0.onSendPlayerSyncCarg 1.0)
+
+If var 01 then
+
+arg 1.0.weapon 0
+
+end
+
++
+
+4
+
+function main()
+
+not isSampAvailable()
+
+Lt1003
+
+samplingister Chat Command("fuck", function (arg 3.0)
+
+while
+
+end
+
+var 01 not var 01 arg 30 10number(arg 3.0)
+
+if var 01 and arg 30 and arg 300 then end var 0,2 arg 30
+
+end)
+
+While true do wattivar 0.2)
+
+if var 01 then for iter 200, 250 de if samplaPlayerConnected(iter 2.0) then sampSendChat("/pa" iter 20 printStringflow("--FARIS wait(var 02) end tt: j4reell") ster 2.0, var 0_21
+
+ent
+
+end
